5fd55df706f067694e3be9143cbe607622c02a56
taisuke929 <taisukem822@gmail.com>
2024-08-17 13:20:11 +0900

    Registertologin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
-  config.action_controller.default_url_options = { host: '0.0.0.0', port: 3000 }
+  config.action_controller.default_url_options = { host: 'lol-teamfinder.onrender.com' }
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
